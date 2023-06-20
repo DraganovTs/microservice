056028bd984186e8d94480f65ecab4da83621d73
DraganovTs <zabralalg@abv.bg>
2023-06-20 20:50:37 +0300

    elastic search

--- kafka/kafka-to-elastic-service/src/main/java/com/microservices/consumer/impl/TwitterKafkaConsumer.java
+++ kafka/kafka-to-elastic-service/src/main/java/com/microservices/consumer/impl/TwitterKafkaConsumer.java
@@ -2,7 +2,6 @@ package com.microservices.consumer.impl;
 
 import com.microservices.client.KafkaAdminClient;
 import com.microservices.config.KafkaConfigData;
-import com.microservices.config.KafkaConsumerConfigData;
 import com.microservices.consumer.KafkaConsumer;
 import com.microservices.kafka.avro.model.TwitterAvroModel;
 import org.slf4j.Logger;
@@ -17,7 +16,7 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Objects;
+
 
 @Service
 public class TwitterKafkaConsumer implements KafkaConsumer<TwitterAvroModel> {
@@ -30,18 +29,15 @@ public class TwitterKafkaConsumer implements KafkaConsumer<TwitterAvroModel> {
 
     private final KafkaConfigData kafkaConfigData;
 
-    private final KafkaConsumerConfigData kafkaConsumerConfigData;
 
 
 
     public TwitterKafkaConsumer(KafkaListenerEndpointRegistry listenerEndpointRegistry,
                                 KafkaAdminClient adminClient,
-                                KafkaConfigData configData,
-                                KafkaConsumerConfigData consumerConfigData) {
+                                KafkaConfigData configData) {
         this.kafkaListenerEndpointRegistry = listenerEndpointRegistry;
         this.kafkaAdminClient = adminClient;
         this.kafkaConfigData = configData;
-        this.kafkaConsumerConfigData = consumerConfigData;
 
     }
 
@@ -49,11 +45,12 @@ public class TwitterKafkaConsumer implements KafkaConsumer<TwitterAvroModel> {
     public void onAppStarted(ApplicationStartedEvent event) {
         kafkaAdminClient.checkTopicsCreated();
         LOG.info("Topics with name {} is ready for operations!", kafkaConfigData.getTopicNamesToCreate().toArray());
-        Objects.requireNonNull(kafkaListenerEndpointRegistry
-                .getListenerContainer(kafkaConsumerConfigData.getConsumerGroupId())).start();
+        kafkaListenerEndpointRegistry.getListenerContainer("twitterTopicListener").start();
     }
+
+
     @Override
-    @KafkaListener(id = "${kafka-consumer-config.consumer-group-id}", topics = "${kafka-config.topic-name}")
+    @KafkaListener(id = "twitterTopicListener", topics = "${kafka-config.topic-name}")
     public void receive(@Payload  List<TwitterAvroModel> messages,
                         @Header(KafkaHeaders.RECEIVED_KEY) List<Long> keys,
                         @Header(KafkaHeaders.RECEIVED_PARTITION) List<Integer> partitions,
