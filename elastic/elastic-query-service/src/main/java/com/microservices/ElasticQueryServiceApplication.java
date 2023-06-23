254d7b984ba42260008313645027dbe66ff92c72
DraganovTs <cvetomir_draganov@abv.bg>
2023-06-23 19:16:16 +0300

    elastic query service config

---
+++ elastic/elastic-query-service/src/main/java/com/microservices/ElasticQueryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class ElasticQueryServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticQueryServiceApplication.class, args);
+    }
+
+}
