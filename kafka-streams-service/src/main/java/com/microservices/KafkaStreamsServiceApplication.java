9a72dbf218eeb0d5c024786d8b89f518e4df97aa
DraganovTs <cvetomir_draganov@abv.bg>
2023-07-21 18:15:06 +0300

    analytic module update

--- kafka-streams-service/src/main/java/com/microservices/KafkaStreamsServiceApplication.java
+++ kafka-streams-service/src/main/java/com/microservices/KafkaStreamsServiceApplication.java
@@ -5,6 +5,7 @@ import com.microservices.runner.StreamsRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -16,15 +17,21 @@ public class KafkaStreamsServiceApplication implements CommandLineRunner {
 
     private final StreamsInitializer streamsInitializer;
 
-    public KafkaStreamsServiceApplication(StreamsRunner<String, Long> streamsRunner, StreamsInitializer streamsInitializer) {
-        this.streamsRunner = streamsRunner;
-        this.streamsInitializer = streamsInitializer;
+    public KafkaStreamsServiceApplication(StreamsRunner<String, Long> runner,
+                                          StreamsInitializer initializer) {
+        this.streamsRunner = runner;
+        this.streamsInitializer = initializer;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaStreamsServiceApplication.class, args);
     }
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
         LOG.info("App starts...");
         streamsInitializer.init();
         streamsRunner.start();
     }
+
 }
