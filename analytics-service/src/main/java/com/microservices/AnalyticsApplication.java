f58c3e067c9c491c164590b77a49bd36c90e9677
DraganovTs <cvetomir_draganov@abv.bg>
2023-07-24 21:13:48 +0300

    eureka discovery service add

--- analytics-service/src/main/java/com/microservices/AnalyticsApplication.java
+++ analytics-service/src/main/java/com/microservices/AnalyticsApplication.java
@@ -1,11 +1,14 @@
 package com.microservices;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AnalyticsApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(AnalyticsApplication.class, args);
     }
+
 }
--- elastic-query-web-client/src/main/java/com/microservices/ElasticQueryWebClientApplication.java
+++ elastic-query-web-client/src/main/java/com/microservices/ElasticQueryWebClientApplication.java
@@ -2,8 +2,9 @@ package com.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
-
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ElasticQueryWebClientApplication {
     public static void main(String[] args) {
